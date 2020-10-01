60a39537c117da9c9f03f04f13e032e1a4b082e1
Murat <89196242960@mail.ru>
2020-10-01 18:09:59 +0300

    initial commit

---
+++ SOVA/SOVA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SOVA
+//
+//  Created by Мурат Камалов on 01.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
